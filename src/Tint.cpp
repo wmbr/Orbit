f5c00ed2a1c2a62bca2f3a7901f0404a945923af
Malte Renken <malte.renken@fu-berlin.de>
2016-02-11 23:37:28 +0100

    Include shaders into final binary instead of keeping them separate.

--- src/Tint.cpp
+++ src/Tint.cpp
@@ -1,11 +1,17 @@
 #include "Tint.hpp"
 
+#include <string>
+
+extern char _binary_shaders_tint_fs_glsl_start;
+extern char _binary_shaders_tint_fs_glsl_end;
+
 Tint::Tint(const sf::Color& color)
 {
 	if(!sf::Shader::isAvailable())
 		throw std::runtime_error("No shader support available");
-	if (!this->shader.loadFromFile("shaders/tint_fs.glsl", sf::Shader::Fragment))
-		throw std::runtime_error("Shader not found");
+	std::string src(&_binary_shaders_tint_fs_glsl_start, &_binary_shaders_tint_fs_glsl_end);
+	if (!this->shader.loadFromMemory(src, sf::Shader::Fragment))
+		throw std::runtime_error("Failed to load shader");
 	this->shader.setParameter("source", sf::Shader::CurrentTexture);
 	this->shader.setParameter("color", color);
 }
